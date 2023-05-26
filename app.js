43addb008ff2a50e82432e5f37672c4f0b52a50b
GKFitz123 <gkf2103@gmail.com>
2023-05-26 03:04:12 -0400

    woked on the links, added the landing page items

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-const sections = document.querySelector('.section');
+const sections = document.querySelectorAll('.section');
 // parent of the btns
-const sectBtns = document.querySelector('.controls');
-const sectBtn = document.querySelector('control');
-const allbtn = document.querySelector.apply('.main-content')
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
 
 
 function PageTransitions(){
-    for(let i =0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', () =>{
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function() {
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', "");
-            this.className += 'active-btn'
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
         })
     }
+    //section active class
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id){
+            //remove 
+            sectBtns.forEach((btn) =>{
+                // console.log(e.target)
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+            sections.forEach((sections) => {
+                sections.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+
+        }
+        
+        
+    })
+    
 }
 
-PageTransitions()
+
+PageTransitions();
 
